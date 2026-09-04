35a47d5cdd79ba30445460e9bfb775e501a0ff65
Daito Manabe <daitomanabe@gmail.com>
2026-09-05 06:33:59 +0900

    Modernize example for Processing 4.5.6

---
+++ p5f_sample/lib/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.rhizomatiks.bvh","l":"BvhBone"},{"p":"com.rhizomatiks.bvh","l":"BvhParser"}];updateSearchResults();
